c5cf2f0d929b7d5c35ed82442bff3a584648350c
FilbertKimasa <filbertdan67@gmail.com>
2023-07-26 02:42:15 +0300

    Add js linters

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const navSlide = () => {
-  const burger = document.querySelector(".bars");
-  const nav = document.querySelector(".menu");
-  const navLinks = document.querySelectorAll(".menu li");
-  const closeMobileNav = document.querySelector(".fa-close");
-  burger.addEventListener("click", () => {
-    nav.classList.add("nav-active");
+  const burger = document.querySelector('.bars');
+  const nav = document.querySelector('.menu');
+  const navLinks = document.querySelectorAll('.menu li');
+  const closeMobileNav = document.querySelector('.fa-close');
+  burger.addEventListener('click', () => {
+    nav.classList.add('nav-active');
   });
   navLinks.forEach((link) => {
-    link.addEventListener("click", function () {
-      navLinks.forEach((li) => nav.classList.remove("nav-active"));
+    link.addEventListener('click', () => {
+      navLinks.forEach(() => nav.classList.remove('nav-active'));
     });
   });
-  closeMobileNav.addEventListener("click", () => {
-    nav.classList.remove("nav-active");
+  closeMobileNav.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
   });
 };
 navSlide();
