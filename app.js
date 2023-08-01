081610bbd06c3b5ba9e8381a838914fba746e6da
FilbertKimasa <filbertdan67@gmail.com>
2023-08-01 13:24:02 +0300

    Modify app.js file

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const navSlide = () => {
   const burger = document.querySelector('.bars');
   const nav = document.querySelector('.menu');
-  const navLinks = document.querySelectorAll('.menu li');
+  const navLinks = document.querySelectorAll('.menu li a');
   const closeMobileNav = document.querySelector('.fa-close');
+  const body = document.getElementById('the-body');
   burger.addEventListener('click', () => {
     nav.classList.add('nav-active');
+    body.classList.add('overflow-hiden');
   });
+
   navLinks.forEach((link) => {
     link.addEventListener('click', () => {
       navLinks.forEach(() => nav.classList.remove('nav-active'));
+      body.classList.remove('overflow-hiden');
     });
   });
   closeMobileNav.addEventListener('click', () => {
     nav.classList.remove('nav-active');
+    body.classList.remove('overflow-hiden');
   });
 };
 navSlide();
